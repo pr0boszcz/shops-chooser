f4c83842f87d62c7004bd3cae6e87b3481cd4103
proboszcz <coolerix@gmail.com>
2016-10-25 09:39:40 +0200

    initial commit with final solution

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # shops-chooser
+Poniżej  znajduje się przykładowy hash z produktami spożywczymi, oraz sklepami, w których można kupić dany produkt w określonej cenie.
+
+    input = {
+        chleb:  {auchan: 2.3, biedronka: 2.6, lidl: 2.4},
+        maslo:  {auchan: 2.5, biedronka: 3, lidl: 2.4, zabka: 3.5},
+        jogurt: {auchan: 1.5, lidl: 1.4, zabka: 2},
+        ser:    {auchan: 1.3, lidl: 1.9, zabka: 1.5, biedronka: 1.1}
+    }
+    cart = [:chleb, :maslo]
+Przykład:  "chleb" można kupić w sklepie "Auchan" za 2,30 zł lub w sklepie  "Biedronka" za 2,60 zł lub w sklepie "Lidl" za 2,40 zł itd. Nie  wszystkie towary można kupić w danym sklepie - np. chleba nie kupię w  “Żabka”.
+
+Zmienna input może zawierać inne produkty, sklepy oraz ceny (w postaci liczby stałej lub zmiennoprzecinkowej). 
+
+Zmienna cart zawiera listę produktów, które chcemy kupić.
+
+
+
+Funkcjonalność I: "Zakupy w jednym sklepie"
+
+Użytkownik chce zrobić zakupy tylko w jednym sklepie. Program dla dowolnej tablicy z produktami (cart) powinien wypisać w jakich sklepach i za ile można kupić dane produkty i posortować je po sumarycznej cenie tych produktów. Sklepy, w których nie można kupić wszystkich produktów powinny zostać pominięte.
+
+Przykład:
+
+Dla powyższych danych (cart = chleb i maslo) program powinien zwrócić następujący wynik:
+
+auchan:
+* chleb 2.3
+* maslo 2.5
+* suma 4.8
+
+lidl:
+* chleb 2.4
+* maslo 2.4
+* suma 4.8
+
+biedronka:
+* chleb 2.6
+* maslo 3
+* suma 5.6
+Wniosek: Chleb i masło będą łącznie kosztować 4,80 w sklepie “Auchan”, 4,80 w sklepie “Lidl”, 5,60 w sklepie “Biedronka”.
+
+W sklepie "Biedronka" użytkownik zapłaci najwięcej, dlatego powinna być na końcu listy.
+
+
+
+Funkcjonalność II: "Zakupy w dowolnych sklepach"
+
+Tym razem użytkownik chce zapłacić najmniej mogąc odwiedzić dowolną ilość sklepów. Program powinien podać listę sklepów, w których zapłaci najmniej za swoje zakupy wraz z listą produktów oraz ich ceną.
+
+Przykład:
+
+Dla powyższych danych (cart = chleb i maslo) program powinien zwrócić następujący wynik:
+
+auchan:
+* chleb 2.3
+
+lidl:
+* maslo 2.4
+Wniosek: Chleb najlepiej kupić w sklepie "Auchan", a masło w sklepie "Lidl".
+
+
+
+Funkcjonalność III: "Zakupy w jednym lub dwóch sklepach"
+
+W tym przypadku użytkownik odwiedza jeden lub dwa sklepy w czasie zakupów. Program powinien napisać do jakich sklepów (maksymalnie dwóch) należy się udać, aby kupić produkty (zmienna cart) możliwie najtaniej.
+
+Przykład 1:
+
+Dla powyższych danych (cart = chleb i maslo) program powinien zwrócić następujący wynik:
+
+auchan:
+* chleb 2.3
+
+lidl:
+* maslo 2.4
+Wniosek: Chleb najlepiej kupić w sklepie "Auchan", a masło w sklepie "Lidl".
+
+Przykład 2:
+
+Jeżeli do koszyka dodamy jeszcze jogurt i ser (cart = chleb, maslo, jogurt, ser) to program powinien zwrócić następujący wynik:
+
+
+lidl:
+* chleb:2.4
+* maslo 2.4
+* jogurt 1.4
+
+biedronka:
+* ser 1.1
+Wniosek: W sklepie "Lidl" najlepiej kupić chleb, masło i jogurt, a w "Biedronka" ser.
---
+++ main.rb
@@ -0,0 +1,199 @@
+# Wybralem, troche przez to, ze nie doczytalem, zadania 1 i 2.
+# Zadanie 1:
+# InputReader.new(input).market.sort_shop_by_prices cart
+# Zadanie 2:
+# InputReader.new(input).market.find_cheapest_products cart
+# Zadanie 3:
+# InputReader.new(input).market.find_cheapest_products_in_n_shops_only cart, n
+
+# Product has information about product itself, its price and shop
+# with this price
+class Product
+  attr_reader :name, :price, :shop
+
+  def initialize(name, price, shop)
+    @price = price.to_f
+    @shop = shop
+    @name = name
+  end
+
+  def to_s
+    " * #{@name}  -> (#{@price})"
+  end
+end
+
+# Shop represents shop with products
+class Shop
+  attr_accessor :name, :products
+
+  def initialize(name, products = nil)
+    @products = []
+    @name = name
+    @products << products
+    @products.compact!
+  end
+
+  def includes_all_products?(cart)
+    cart - products_names_list == []
+  end
+
+  def sum_of_cart(cart)
+    if includes_all_products?(cart)
+      products_list = select_products_for_sum(cart)
+      products_list.map(&:price).inject(:+)
+    end
+  end
+
+  def select_products_for_sum(cart)
+    products_list = @products - [[]]
+    products_list.compact.select { |item| cart.include? item.name }
+  end
+
+  private
+
+  def products_names_list
+    products_names = @products - [[]]
+    products_names.compact.map(&:name)
+  end
+
+  def to_s
+    "#{@name}:"
+  end
+end
+
+# InputReader - Class for read and parse input
+class InputReader
+  attr_accessor :market
+  def initialize(input)
+    @market = Market.new
+    input.each do |product, shops|
+      shops.each do |shop, price|
+        shoptmp = pick_shop(shop)
+        producttmp = Product.new(product, price, shoptmp)
+        shoptmp.products << producttmp
+        @market.shops << shoptmp unless shop_exists?(shop)
+      end
+    end
+  end
+
+  def shop_exists?(shop)
+    @market.shops.map(&:name).include? shop
+  end
+
+  def pick_shop(shop)
+    return Shop.new(shop) unless shop_exists?(shop)
+    @market.shops.select { |item| item.name == shop }.first
+  end
+end
+
+# Usefull for parsing output - represents ShoppingList ;)
+class ShoppingList
+  attr_accessor :shop, :products
+
+  def initialize(shop, products)
+    @shop = shop
+    @products = products
+  end
+
+  def sum
+    products.map(&:price).inject(:+)
+  end
+end
+
+# update Array with sum I need
+class Array
+  def sum
+    map(&:sum).inject(:+).round(2)
+  end
+end
+
+# Market - Contener of Shops
+class Market
+  attr_accessor :shops
+
+  def initialize
+    @shops = []
+  end
+
+  def sort_shops_by_prices_sum(cart, shops = @shops)
+    shops = shops.select { |shop| shop.includes_all_products?(cart) }
+    shops_list = []
+    shops.each do |shop|
+      products = shop.select_products_for_sum(cart)
+      shops_list << ShoppingList.new(shop, products)
+    end
+    print_shopping_list(shops_list, :sort)
+  end
+
+  def find_cheapest_products(cart)
+    cheapest = select_and_group_cheapest_products(@shops, cart)
+    print_shopping_list(prepare_products_shopping_list(cheapest))
+  end
+
+  def find_cheapest_products_in_n_shops_only(cart, nshops)
+    shops_combs = prepare_shops_combinations_with_cart_products(cart, nshops)
+    cheapest = []
+    shops_combs.each do |shops|
+      products = select_and_group_cheapest_products(shops, cart)
+      cheapest << ShoppingList.new(shops, products)
+    end
+    cheapest_products = cheapest.sort_by(&:sum).first.products
+    list = prepare_products_shopping_list(cheapest_products)
+    print_shopping_list(list)
+  end
+
+  private
+
+  def prepare_products_shopping_list(products)
+    lists = []
+    products.group_by(&:shop).each do |shop, items|
+      lists << ShoppingList.new(shop, items)
+    end
+    lists
+  end
+
+  def all_products_from_n_shops(shops)
+    shops.map(&:products).flatten
+  end
+
+  def all_products_from_n_shops_by_cart(shops, cart)
+    all_products_from_n_shops(shops).select { |item| cart.include? item.name }
+  end
+
+  def shops_includes_all_products?(shops, cart)
+    cart - all_products_from_n_shops(shops).map(&:name) == []
+  end
+
+  def prepare_shops_combinations_with_cart_products(cart, nshops)
+    shops_combs = @shops.combination(nshops)
+    shops_combs.select do |shops|
+      shops_includes_all_products?(shops, cart)
+    end
+  end
+
+  def choose_cheapest_product(products)
+    products.min { |a, b| a.price <=> b.price }
+  end
+
+  def select_and_group_cheapest_products(shops, cart)
+    cheapest = []
+    all_products_from_n_shops_by_cart(shops, cart).group_by(&:name).each do |product|
+      cheapest << choose_cheapest_product(product[1])
+    end
+    cheapest
+  end
+
+  def print_shopping_list(products, *opt)
+    products.each do |item|
+      puts item.shop
+      item.products.each { |product| puts product }
+      puts sumstr(item) if opt.include? :sort
+    end
+    puts sumstr(products) unless opt.include? :sort
+  end
+
+  def sumstr(item)
+    sumpref = '====== suma: '
+    sumpref + item.sum.to_s
+  end
+end
